79767a6493ea65abe86c4a1e43722865eb97c6c2
ohexa <kcxyz.xyz@gmail.com>
2025-05-26 22:49:14 +0800

    major: use hex as pagination

--- src/lib/keys.search.ts
+++ src/lib/keys.search.ts
@@ -2,8 +2,8 @@ import { z } from 'zod'
 
 export const keysSearch = z.object({
   style: z.enum(['ae2td', 'addr1']),
-  page: z.number().default(1),
-  entropySize: z.number().default(12),
+  hex: z.string().default('00000000000000000000000000000000'),
+  size: z.number().default(12),
 })
 
 export type KeysSearch = z.infer<typeof keysSearch>
--- src/routes/keys.tsx
+++ src/routes/keys.tsx
@@ -9,7 +9,6 @@ import { ChevronDownIcon } from 'lucide-react'
 import { useMemo } from 'react'
 
 import type { AddressEntity } from '@/lib/address.entity'
-import type { EntropySize } from '@/lib/entropy.size'
 import type { ColumnDef } from '@tanstack/react-table'
 
 import { Button } from '@/components/ui/button'
@@ -30,7 +29,6 @@ import {
 } from '@/components/ui/table'
 import { addressIcarus } from '@/lib/address.icarus'
 import { addressShelley } from '@/lib/address.shelley'
-import { entropyMap } from '@/lib/entropy.map'
 import { keysSearch } from '@/lib/keys.search'
 
 export const Route = createFileRoute('/keys')({
@@ -40,47 +38,57 @@ export const Route = createFileRoute('/keys')({
 
 function RouteComponent() {
   const pageSizeN = 20n
+  const { style, hex, size } = Route.useSearch()
 
-  const { style, page, entropySize } = Route.useSearch()
+  const sizeToEntropyBits = {
+    '12': 128n,
+    '15': 160n,
+    '24': 256n,
+  } as const
+  const entropyBits =
+    sizeToEntropyBits[size as unknown as keyof typeof sizeToEntropyBits]
 
-  const pageN = BigInt(page)
+  const padLength = Number(entropyBits) / 4
+
+  const hexStr = hex || '0'.repeat(padLength)
+  const hexValue = BigInt(`0x${hexStr}`)
+
+  const maxHexValue = (1n << entropyBits) - 1n
+  const clampedHexValue = hexValue > maxHexValue ? maxHexValue : hexValue
+  const toHexString = (val: bigint) => val.toString(16).padStart(padLength, '0')
+
+  const nextHexValue = clampedHexValue + pageSizeN
+  const prevHexValue =
+    clampedHexValue >= pageSizeN ? clampedHexValue - pageSizeN : 0n
+  const isMaxReached = nextHexValue > maxHexValue
+
+  const totalPages = (maxHexValue + 1n + pageSizeN - 1n) / pageSizeN
+  const lastPageStartValue = (totalPages - 1n) * pageSizeN
+  const lastHex = toHexString(lastPageStartValue)
+
+  const lastPageSize = Number(maxHexValue - lastPageStartValue + 1n)
+  const isLastPage = clampedHexValue === lastPageStartValue
+  const currentPageSize = isLastPage ? lastPageSize : Number(pageSizeN)
+
+  const currentPage = clampedHexValue / pageSizeN + 1n
 
   const data: Array<AddressEntity> = useMemo(() => {
-    const bits = entropyMap.get(BigInt(entropySize) as EntropySize)
-    const start = (pageN - 1n) * pageSizeN
-    const length = Number(bits) / 4
+    return Array.from({ length: currentPageSize }, (_, i) => {
+      const index = clampedHexValue + BigInt(i)
+      const hexadecimal = index.toString(16).padStart(padLength, '0')
 
-    return Array.from({ length: Number(pageSizeN) }, (_, i) => {
-      const index = start + BigInt(i)
-      const hexadecimal = index.toString(16).padStart(length, '0')
+      const address =
+        style === 'ae2td'
+          ? addressIcarus(hexadecimal)
+          : addressShelley(hexadecimal)
 
-      let address: string
-
-      switch (style) {
-        case 'ae2td':
-          address = addressIcarus(hexadecimal)
-          break
-        case 'addr1':
-          address = addressShelley(hexadecimal)
-          break
-      }
-
-      return {
-        hexadecimal,
-        address,
-      } satisfies AddressEntity
+      return { hexadecimal, address } satisfies AddressEntity
     })
-  }, [page, entropySize, style])
+  }, [clampedHexValue, style, currentPageSize])
 
   const columns: Array<ColumnDef<AddressEntity>> = [
-    {
-      accessorKey: 'hexadecimal',
-      header: 'Private Key',
-    },
-    {
-      accessorKey: 'address',
-      header: 'Address',
-    },
+    { accessorKey: 'hexadecimal', header: 'Private Key' },
+    { accessorKey: 'address', header: 'Address' },
   ]
 
   const table = useReactTable({
@@ -92,78 +100,85 @@ function RouteComponent() {
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex flex-col sm:flex-row gap-4 items-center justify-between">
+      <div className="flex flex-col justify-between items-center gap-4 sm:flex-row">
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button>
-              Entropy Size: {entropySize}
+              Entropy Size: {size}
               <ChevronDownIcon />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>Select entropy size</DropdownMenuLabel>
-            {[12, 15, 24].map((n: number) => (
-              <DropdownMenuItem asChild>
-                <Link
-                  to={'/keys'}
-                  search={{
-                    ...Route.useSearch(),
-                    entropySize: n,
-                  }}
-                >
-                  {n}
-                </Link>
-              </DropdownMenuItem>
-            ))}
+            {[12, 15, 24].map((n) => {
+              const zeroHex = '0'.repeat(padLength)
+
+              return (
+                <DropdownMenuItem asChild key={n}>
+                  <Link
+                    to="/keys"
+                    search={{
+                      ...Route.useSearch(),
+                      size: n,
+                      hex: zeroHex,
+                    }}
+                  >
+                    {n}
+                  </Link>
+                </DropdownMenuItem>
+              )
+            })}
           </DropdownMenuContent>
         </DropdownMenu>
-        <div className="flex flex-col sm:flex-row gap-4 items-center">
-          <Link
-            to={'/keys'}
-            search={{
-              ...Route.useSearch(),
-              page: Number(pageN - 1n),
-            }}
-          >
-            <Button disabled={pageN === 1n}>Previous</Button>
+
+        <div className="flex gap-2">
+          <Link to="/keys" search={{ style, size, hex: toHexString(0n) }}>
+            <Button disabled={clampedHexValue === 0n}>First</Button>
           </Link>
+
           <Link
-            to={'/keys'}
-            search={{
-              ...Route.useSearch(),
-              page: Number(pageN + 1n),
-            }}
+            to="/keys"
+            search={{ style, size, hex: toHexString(prevHexValue) }}
           >
-            <Button>Next</Button>
+            <Button disabled={clampedHexValue === 0n}>Previous</Button>
+          </Link>
+
+          <Link
+            to="/keys"
+            search={{ style, size, hex: toHexString(nextHexValue) }}
+          >
+            <Button disabled={isMaxReached}>Next</Button>
+          </Link>
+
+          <Link to="/keys" search={{ style, size, hex: lastHex }}>
+            <Button disabled={clampedHexValue === lastPageStartValue}>
+              Last
+            </Button>
           </Link>
         </div>
       </div>
+
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                  </TableHead>
-                )
-              })}
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                </TableHead>
+              ))}
             </TableRow>
           ))}
         </TableHeader>
         <TableBody>
           {table.getRowModel().rows.length ? (
             table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && 'selected'}
-              >
+              <TableRow key={row.id}>
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
@@ -173,13 +188,16 @@ function RouteComponent() {
             ))
           ) : (
             <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
+              <TableCell colSpan={columns.length} className="text-center h-24">
                 No results.
               </TableCell>
             </TableRow>
           )}
         </TableBody>
       </Table>
+      <div className="text-xs">
+        {currentPage.toString()} of {totalPages.toString()}
+      </div>
     </div>
   )
 }
